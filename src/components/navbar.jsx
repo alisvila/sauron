c1c253c4cb2866bd01c0e967f5887cd5dc82c6a2
ali attar <aliam1991@gmail.com>
2019-07-03 16:15:26 +0430

    improve some components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,25 +4,11 @@ import { Link, BrowserRouter as Router } from 'react-router-dom';
 const LoggedInView = props => {
   if(!props.currentUser) {
     return(
-      <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item nav-btn">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
     )
   }
   return null
@@ -31,28 +17,12 @@ const LoggedInView = props => {
 const LoggedOutView = props => {
   if(props.currentUser) {
     return(
-      <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
+
+      <ul class="navbar-nav">
+        <li class="nav-item nav-btn">
+          <a class="nav-link" href="#">Admin Console</a>
+        </li>
+      </ul>
     )
   }
   return null
@@ -89,15 +59,33 @@ class NavBar extends Component {
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>
+
             <LoggedOutView currentUser={this.props.currentUser} />
 
             <LoggedInView currentUser={this.props.currentUser} />
 
-            <ul class="navbar-nav">
-                <li class="nav-item nav-btn">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-              </ul>
 
             </div>
           </nav>
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { applyMiddleware,createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './reducer';
