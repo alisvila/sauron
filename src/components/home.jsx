c0824414461db76e00bc517a9420a0fda1031eda
ali attar <aliam1991@gmail.com>
2019-07-28 20:07:09 +0430

    home page design

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,137 +1,173 @@
 import React, { Component } from 'react'
 import Navbar from './navbar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class Home extends Component {
     render() {
         return (
             <>
                 <Navbar />
-                <div className="container">
-                    <img className="img-fluid" src="/img/main-banner.jpg" />
-                    <div className="banner-text-wrapper">
-                        <div className="row">
-                            <span className="banner-text">
-                                some text
-                            </span>
-                        </div>
-                        <div className="row">
-                            <a className="btn btn-primary">click</a>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="sec-colom">
-                            <div className="col-md-12">
-                                <img className="hero-logo" src="https://www.google.com/recaptcha/intro/images/hero-recaptcha-invisible.gif" />
+                <div className="container-fluid back-color">
+                    <div class="row banner">
+                        <div className="banner-text-wrapper">
+                            <div className="row">
+                                <span className="banner-text">
+                                    some text
+                                </span>
                             </div>
-                            <div className="col-md-12">
-                                <h2>Introducing naKhanaa v1</h2>
-                            </div>
-                            <div className="col-md-12">
-                                <p>
-                                We are excited to introduce reCAPTCHA v3, which helps you detect abusive traffic on your website without any user friction. It returns a score based on the interactions with your website and provides you more flexibility to take appropriate actions. See blog for more details.
-                                </p>
+                            <div className="row">
+                                <a className="btn btn-primary">click</a>
                             </div>
                         </div>
                     </div>
 
-                    <div className="row">
-                        <div className="sec-colom">
-                            <div className="col-md-12">
-                                <h1 className="header-question">What is reCAPTCHA?</h1>
-                            </div>
-                            <div className="col-md-12">
-                                <p>
-                                    reCAPTCHA is a free service that protects your website from spam and abuse. reCAPTCHA uses an advanced risk analysis engine
-                                    and adaptive challenges to keep automated software from engaging in abusive activities on your site. It does this while letting
-                                    your valid users pass through with ease.
-                                </p>
+
+                    <div className="row banner-extension section">
+                        <div className="container box-shadow avalin-section">
+                            <div className="row">
+                                <div className="col-lg-4 pillar solo-pillar">
+                                    <h2>لورم ایپسوم امه و مجله در ستون </h2>
+                                    <p> برای شرایط فعلی تکنولوژی مورد نیاز و  است و برای شرایط فعلی تکنولوژی مورد نیاز و کا  است و برای شرایط فعلی تکنولوژی مورد نیاز و کا کاربردهای متنوع با هدف</p>
+                                    <a href="/advertisers/intro/" className="btn-finno more-btn hover-pulse-animate"><i className="fa fa-chevron-circle-right" aria-hidden="true"></i>بیشتر بدانید</a>
+                                </div>
+                                <div className="col-lg-4 pillar">
+                                    <img className="sh-image-l" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/07/benifits-1.png" alt="benifits-1" />
+                                    <h2>لورم ایپسوم </h2>
+                                    <p>
+                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف                                    </p>
+                                </div>
+                                <div className="col-lg-4 pillar">
+                                    <img className="sh-image-l" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/07/benifits-2.png" alt="benifits-2" />
+                                    <h2>لورم ایپسوم </h2>
+                                    <p>
+                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="row banner-extension">
+                    <div className="features row justify-content-center section">
                         <div className="container">
                             <div className="row">
-                                <div className="col-lg-6">
-                                    <h2>تبلیغ‌دهنده </h2>
-                                    <p>
-                                    تبلیغات موبایلی از طریق امکانات هدفمندی، بیش از هر روش تبلیغاتی دیگر، قادر است شما را به مشتریان بالقوه نزدیک کند. همچنین با ارائه آمار و ارقام دقیق شما را در تجزیه و تحلیل مسیر بازاریابی کمک خواهد کرد.
-                                    </p>
-                                    <a href="/advertisers/intro/" className="more-btn hover-pulse-animate"><i className="fa fa-chevron-circle-right" aria-hidden="true"></i>بیشتر بدانید</a>
+                                <div className="col-md-4 services-wrap fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                                        <div className="feature-wrap  box-shadow pillar">
+                                            <div className="center-text content">
+                                                <h3>لورم ایپسوم</h3>
+                                                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است برای شرایط فعلی تکنولوژی برای تکنولوژی مورد</p>
+                                            </div>
+                                            <img class="sh-image-url" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/03/insurance.jpg" alt="insurance" />
+                                        </div>
                                 </div>
-                                <div className="col-lg-6">
-                                    <h2>نمایش‌دهنده </h2>
-                                    <p>
-                                    عدد نماینده رسمی تبلیغات در اپلیکیشن‌های منتشر شده در کافه بازار می باشد. برای عدد حفظ رضایت کاربر از اپلیکیشن شما یک اولویت است. شما می‌توانید به راحتی با پیاده سازی کلاینت عدد و نمایش تبلیغات در اپلیکیشن خود، به ازای کلیک کاربران، درآمد کسب کنید.
-                                    </p>
-                                    <a href="/publishers/intro/" className="more-btn hover-pulse-animate"><i className="fa fa-chevron-circle-right" aria-hidden="true"></i>بیشتر بدانید</a>
+                                <div className="col-md-4" data-wow-duration="1000ms" data-wow-delay="600ms">
+                                        <div className="feature-wrap box-shadow pillar">
+                                            <div className="center-text content">
+                                                <h3>فعلی تکنولوژی</h3>
+                                                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است برای شرایط فعلی تکنولوژی مورد</p>
+                                            </div>
+                                                <img class="sh-image-url" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/07/we-offer-2-1.png" alt="we-offer-2-1" />
+                                        </div>
                                 </div>
+                                <div className="col-md-4" data-wow-duration="1000ms" data-wow-delay="600ms">
+                                        <div className="feature-wrap box-shadow pillar">
+                                            <div className="center-text content">
+                                                <h3> طراحان گرافیک</h3>
+                                                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است برای شرایط فعلی تکنولوژی مورد</p>
+                                            </div>
+                                                <img class="sh-image-url" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/03/undraw_folder_39kl.jpg" alt="undraw_folder_39kl" />
+                                        </div>
+                                </div>                           
                             </div>
                         </div>
                     </div>
 
-                    <div className="features row justify-content-center">
-                        <div className="col-md-3 services-wrap fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
-                            <a href="http://card.sabinarya.com/">
-                                <div className="feature-wrap">
-                                    <div className="center-text">
-                                        <p>filan</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3" data-wow-duration="1000ms" data-wow-delay="600ms">
-                            <a href="https://finnoboom.com/">
-                                <div className="feature-wrap">
-                                    <div className="center-text">
-                                        <p>filan</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="col-md-3" data-wow-duration="1000ms" data-wow-delay="600ms">
-                            <a href="http://aryapay24.com/">
-                                <div className="feature-wrap">
-                                    <div className="center-text">
-                                        <p>filan</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="">
+                    <div className="row section change-color">
+                    <div className="container">
                         <div className="row">
-                            <div className="col-md-4">
-                                <img className="icons" alt="" src="images/icon-advanced.png" />
-                                <h2>
-                                    Advanced Security
-                                </h2>
-                                <p>
-                                    State of the art spam &amp; abuse protection for your website
-                                </p>
+                            <div className="col-md-7 sep">
+                                <img className="" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/07/driven-to-deliver.png" alt="driven-to-deliver" />
                             </div>
-                            <div className="col-md-4">
-                                <img className="icons" alt="" src="images/icon-ease.png" />
-                                <h2>
-                                    Ease of Use
-                                </h2>
-                                <p>
-                                    Low friction, effortless interaction for your users
-                                </p>
+                            <div className="col-md-5 sep">
+                                <h2>لورم ایپسوم متن ساختگی</h2>
+                                <span className="sub-header">لورم ایپسوم متن ساختگی</span>
+                                <div className="short-row">
+                                <h3> سادگی نامفهوم</h3>
+                                <p>لورم ایپسوم متن ساختگی لورم ایپسوم متن ساختگی لورم ایپسوم متن ساختگی</p>
+                                </div>
+                                <div className="short-row">
+                                <h3>گرافیک</h3>
+                                <p>لورم ایپسوم متن ساختگی لورم ایپسوم متن ساختگی لورم ایپسوم متن ساختگی</p>
+                                </div>
+                                <div className="short-row">
+                                <h3>لورم ایپسوم</h3>
+                                <p>لورم ایپسوم متن ساختگی لورم ایپسوم متن ساختگی لورم ایپسوم متن ساختگی</p>
+                                </div>
                             </div>
-                            <div className="col-md-4">
-                                <img className="icons" alt="" src="images/icon-creation.png" />
-                                <h2>
-                                    Creation of Value
-                                </h2>
-                                <p>
-                                    Apply the human bandwidth to benefit people everywhere
-                                </p>
+
+                        </div>
+                    </div>
+                    </div>
+                    <div className="row section background-image">
+                        <div className="container">
+                            <div className="row">
+                                <h2>Subscribe to our newsletter</h2>
+                                <p>You will receive newest information about our projects, investments</p>
+                                <p>partners and other important information.</p>
                             </div>
                         </div>
                     </div>
+
+                    <footer className="row">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-3 footer-pillar">
+                                <img class="logo-image" src="https://cdn.jevelin.shufflehound.com/wp-content/uploads/sites/26/2019/03/finances-logo.png" alt="finances-logo" />
+                                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله</p>
+                                </div>
+                                <div className="col-md-3 footer-pillar">
+                                    <h4>filan</h4>
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">FAQ</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Cookies</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Privacy Policy</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="col-md-3 footer-pillar">
+                                <h4>filan</h4>
+                                <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Insurance</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Planning</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Consulting</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="col-md-3 footer-pillar">
+                                <h4>filan</h4>
+                                <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Streed address 15, City</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">+321 97654387</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">emailaddress@company.com</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
                 </div>
             </>
         )
